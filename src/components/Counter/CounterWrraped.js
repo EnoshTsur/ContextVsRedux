75a6b730e2f3a2d589f01e0c01181d2472ff12a1
Enosh Tsur <enoshh12@gmail.com>
2020-06-21 22:32:16 +0300

    refactor name

--- src/components/Counter/CounterWrraped.js
+++ src/components/Counter/CounterWrraped.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function HomePage({ counter, increase, decrease, }) {
+export default function CounterWrraped({ counter, increase, decrease, }) {
 
     return (
         <div>
